b612210715c96bb653bce2151c1d6d4a997bb0fd
Will Drexler <wdrexler@mojolingo.com>
2021-03-31 18:14:47 -0500

    Documents

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,13 +1,14 @@
 class DocumentsController < ApplicationController
   include MeetingsHelper
   before_action :set_meeting
-  before_aciton :set_document, except: [:index, :new, :create]
+  before_action :set_document, except: [:index, :new, :create]
   before_action :require_meeting_member!
   before_action :cannot_modify_signed!, only: [:edit, :update, :destroy]
   before_action :require_current_account_admin, except: [:index, :show, :pdf, :download]
 
   # GET /meetings/:meeting_id/documents
   def index
+    @host = current_account_admin?
     @documents = @meeting.documents
   end
 
@@ -19,9 +20,10 @@ class DocumentsController < ApplicationController
   # POST /meetings/:id/documents
   def create
     @document = @meeting.documents.new(document_params)
+    @document.created_by = current_user
 
     if @document.save
-      redirect_to @meeting, notice: t("documents.notice.create")
+      redirect_to new_meeting_documents_path(@meeting), notice: t("documents.notice.create")
     else
       render :new, status: :unprocessable_entity
     end
