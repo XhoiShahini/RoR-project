73aff9a09014fe18b690b6408f44abe87d40ff6b
Will Drexler <wdrexler@mojolingo.com>
2021-04-01 17:02:07 -0500

    Meeting list page

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -6,11 +6,11 @@ class MeetingsController < ApplicationController
 
   # GET /meetings
   def index
-    # TODO Ransack
-    @pagy, @meetings = pagy(Meeting.sort_by_params(params[:sort], sort_direction))
+    @q = current_account.meetings.ransack(params[:q])
+    @meetings = @q.result(distinct: true)
+    @pagy, @meetings = pagy(@meetings.sort_by_params(params[:sort], sort_direction))
 
     # We explicitly load the records to avoid triggering multiple DB calls in the views when checking if records exist and iterating over them.
-    # Calling @sms_verifications.any? in the view will use the loaded records to check existence instead of making an extra DB call.
     @meetings.load
   end
 
