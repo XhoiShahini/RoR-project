d8f2a66f11e1eb13ac9cdd33c56ef40942eb1745
Will Drexler <wdrexler@mojolingo.com>
2021-04-30 18:25:49 -0500

    For now, bypass CSRF with JSON requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception, except: :phone_input_utils
+  protect_from_forgery with: :exception, except: :phone_input_utils, unless: -> { request.format.json? }
 
   include SetCurrentRequestDetails
   include SetLocale
